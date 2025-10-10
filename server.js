4c41e3d2444edc78c1d042ed71d707cef8e36789
3bdo-Gamal <abdalrhmanGamaleldien@gmail.com>
2025-10-11 00:11:11 +0300

    smallerror

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-const credentials = JSON.parse(fs.readFileSync("process.env.GOOGLE_CREDENTIALS"));
+const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
 
 const auth = new google.auth.GoogleAuth({
   credentials,
